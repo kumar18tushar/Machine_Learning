d1c8ed0697a5a475b7c5cf13033861d22e84f2db
kumar18tushar <kumar18tushar@gmail.com>
2019-02-25 19:47:45 +0530

    Linear Regression done

---
+++ Linear_Regression/Linear_Regression(multi-variate)/linear_regression(multi-variate).py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+
+
+def compute_cost(x,y,theta):
+        m=len(x)
+        prod = np.subtract(np.matmul(x,theta),y)
+        temp=np.square(prod)
+        su = (temp.sum(axis=0))*(1/(2*m))
+        return(su)
+        
+
+
+
+
+
+
+def gradient_descent(x,y,theta,alph,iter):
+        
+        xaxis=np.zeros((2000))
+        yaxis=np.zeros((2000))
+     
+        for i in range(iter):
+                prod = np.subtract(np.matmul(x,theta),y)
+                m=len(prod)
+                prod.shape=(1,m)
+                temp = np.matmul(prod,x)
+                temp.shape=(len(theta),1)
+                theta=theta-(alph*1/(m))*temp
+                yaxis[i]=compute_cost(x,y,theta)
+                xaxis[i]=i
+
+
+        return theta
+        #plt.plot(xaxis,yaxis)
+        #plt.show()
+       
+                
+                
+
+
+
+#def show_plot(t1,t2,y):
+        
+        
+
+
+
+def main():
+        t1,t2,y=np.loadtxt('ex1data2.txt',delimiter=',',unpack=True)
+
+        
+        
+        me1=np.mean(t1)
+        me2=np.mean(t2)
+        me3=np.mean(y)
+
+        d1=np.ptp(t1,axis=0)
+        d2=np.ptp(t2,axis=0)
+        d3=np.ptp(y,axis=0)
+        
+        m=len(t1)
+        x=np.zeros((m,2))
+
+        for i in range(m):
+            x[i][0]=t1[i]=(t1[i]-me1)/d1
+            x[i][1]=t2[i]=(t2[i]-me2)/d2
+            y[i]=(y[i]-me3)/d3
+
+        #show_plot(t1,t2,y)
+
+        xdash=x
+        
+        x=np.insert(x,0,1,axis=1)
+        y.shape=(m,1)
+        
+        theta = np.zeros((3,1))
+        alph=0.04
+        iter=2000
+
+
+        #theta_val=gradient_descent(x,theta,alpha,iter)
+        compute_cost(x,y,theta)                                       
+        theta_val = gradient_descent(x,y,theta,alph,iter)
+
+        print(theta_val)
+
+        re=np.matmul(x,theta_val)
+        print(re)
+        print(y)
+        
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(t1 , t2 ,y, alpha=1.0, c='green',marker='.')
+        ax.scatter3D(t1, t2, re,alpha=1.0, c='red',marker='x')
+        #ax.plot(t1,t2,y)
+        plt.show()
+        
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ Linear_Regression/Linear_Regression(single-variate)/linear_regression.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def compute_costFunction(x,y,theta):
+    m=len(x)
+    prod = np.subtract(np.matmul(x,theta),y)
+    temp=np.square(prod)
+    sum_matrix = (temp.sum(axis=0))*(1/(2*m))
+    return sum_matrix
+    
+
+
+
+def gradient_descent(x,y,theta,alpha,iter):
+    for i in range(iter):
+        m=len(x)
+        prod1 = np.subtract(np.matmul(x,theta),y)
+        l=len(prod1)
+        prod1.shape=(1,l)
+        prod2=np.matmul(prod1,x)
+        prod2.shape=(2,1)
+        theta=theta-(alpha*1/(m))*prod2
+        cost=compute_costFunction(x,y,theta)
+    return theta
+    
+
+
+def visualise(x,y):
+    thetaval0 = np.linspace(-10,10,num=100)
+    thetaval1 = np.linspace(-1,4,100)
+    jval=np.zeros((100,100),dtype=int)
+
+    for i in range(len(thetaval0)):
+        for j in range(len(thetaval1)):
+            th=np.array([thetaval0[i],thetaval1[j]])
+            th.shape=(2,1)
+            b=compute_costFunction(x,y,th)
+            jval[i][j]=b
+
+    np.transpose(jval)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    Axes3D.plot_surface(ax,thetaval0, thetaval1, jval)
+    plt.show()
+    #ax = fig.add_subplot(111, projection='3d')
+    #Axes3D.plot_surface(thetaval0 , thetaval1 , jval)
+
+
+
+
+def show_plot(x,y):
+	plt.scatter(x , y , alpha=0.8, c='red',marker='x')
+	plt.xlabel('Population of City in 10,000s')
+	plt.ylabel('Profit in $10,000s')
+	#plt.show() 
+
+
+
+
+
+
+
+def main():
+        x, y = np.loadtxt('ex1data1.txt',delimiter=',',unpack=True)         #creates a array(list) of x and y from ex1data1.txt
+        m=len(x)
+        x.shape =(m,1)                                                      #changes x[] (1 x m) into dimension (m x 1)
+        xtemp=x
+        y.shape=(m,1)
+        x=np.insert(x,0,1,axis=1)                                           #insert 1 at position 0 rowise https://stackoverflow.com/questions/36998260/prepend-element-to-numpy-array
+
+        theta_init = np.array([0,0])
+        theta_init.shape=(2,1)
+        alpha=0.01
+        iter=1500
+        
+        theta_val = gradient_descent(x,y,theta_init,alpha,iter)
+
+        re=np.matmul(x,theta_val)
+        visualise(x,y)
+        #show_plot(xtemp,y)
+        #plt.plot(xtemp,re)
+        #plt.show()
+
+       
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#sum_matrix=x.sum(axis=0) means:
+# X = [[1],[2],[3]] then sum_matrix = [6]
+
+#sum_matrix=x.sum(axis=1) means:
+# X = [[1],[2],[3]] then sum_matrix = [1,2,3]
+
+#store the value of x transpose in p without changing x
+#p=np.transpose(x)
